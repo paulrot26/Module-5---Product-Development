9ca28e45242177a9a6a4740d9e6daa183e4a5dbe
paul <pwrothwell@hotmail.com>
2026-05-06 11:37:51 +0100

    Day 2 Update 11:37

--- Day 2/data_cleaner.py
+++ Day 2/data_cleaner.py
@@ -1,7 +1,10 @@
-
+import pandas as pd
 
 df = pd.read_csv("03_Library Systembook.csv")   
 df
+
+df1 = pd.read_csv("03_Library SystemCustomers.csv")   
+df1
 
 
 df["Books"] = df["Books"].str.title()
@@ -9,15 +12,11 @@ df["Books"] = df["Books"].str.title()
 df["Books"]
 
 df = df.dropna(subset=["Books"])
+df1 = df1.dropna()
 
 df["Book checkout"] = df["Book checkout"].replace({'"': ''}, regex=True)
 
 df["Book checkout_valid"] = pd.to_datetime(df["Book checkout"], errors="coerce")
-
-df1 = pd.read_csv("03_Library SystemCustomers.csv")   
-df1
-
-df1 = df1.dropna()
 
 df["Book checkout"] = pd.to_datetime(df["Book checkout"], errors="coerce")  
 df["Book Returned"] = pd.to_datetime(df["Book Returned"], errors="coerce")
@@ -28,6 +27,23 @@ df["Book Due Back"] = pd.to_datetime(df["Book Due Back"], errors="coerce")
 
 df["Overdue"] = df["Book Returned"] > df["Book Due Back"]
 
+# Creating and using a function to enrich the data by adding in the time a book was on loan.
+data_enriched = df.copy()
 
-df.to_csv("Cleaned_03_Library Systembook.csv", index=False)
+def enrich_dateDuration(colA, colB, df=data_enriched):
+    """
+    Takes the two input columns and the dataframe to create a new column date_delta which is the difference, in days, between colA and colB.
+    
+    Note: ColA should be the highest of the expected date columns.
+    """
+    df['date_delta'] = (df[colA]-df[colB]).dt.days
+    return df.head()
+
+enrich_dateDuration(df=data_enriched, colA='Book Returned', colB='Book checkout')
+
+data_enriched = df.copy()
+data_enriched['loan_duration'] = (data_enriched['Book Returned'] - data_enriched['Book checkout']).dt.days
+data_enriched.head()
+
+data_enriched.to_csv("Cleaned_03_Library Systembook.csv", index=False)
 df1.to_csv("Cleaned_03_Library SystemCustomers.csv", index=False)
