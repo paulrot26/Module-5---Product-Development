32f053289ad404e0e064b60d11612be8b4b3c1a6
paul <pwrothwell@hotmail.com>
2026-05-07 11:02:07 +0100

    day3 morning break

---
+++ Day 3/Docker_demo_1/app.py
@@ -0,0 +1,1 @@
+print("hellow world, i just used docker")
---
+++ Day 3/Docker_practice_calc/calculator.py
@@ -0,0 +1,10 @@
+import math
+class Calculator:
+    
+        # Square root of 435
+    def get_square_root_435(self):
+        return math.sqrt(435)
+        
+    
+myCalc = Calculator()
+print(myCalc.get_square_root_435())
